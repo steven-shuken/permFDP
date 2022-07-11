120e6ac48de2a923f32d45158535a47d4cc760e1
steven-shuken <srshuken@gmail.com>
2022-07-11 15:59:53 -0400

    Added comments for permFDP.R

--- R/permFDP.R
+++ R/permFDP.R
@@ -3,20 +3,25 @@ library(BH)
 Sys.setenv("PKG_CXXFLAGS"="-std=c++17")
 sourceCpp("../src/PermFDRAdjust.cpp")
 
-#' A Cat Function
+#' Permutation-Based FDP Method for Rejection Threshold Correction
 #'
-#' This function allows you to express your love of cats.
-#' @param love Do you love cats? Defaults to TRUE.
-#' @keywords cats
+#' This function controls FDR using the permutation method described in our manuscript. Like the BH method above, it corrects the rejection threshold rather than the p-values themselves.
+#' It returns the new threshold for P-value rejection.
+#' It uses the Rcpp and BH packages to leverage fast C++ code.
+#' @param pVals Vector of p-values. The length of this vector must be the same as the number of rows in the intOnly data frame
+#' @param threshold Original threshold that will be adjusted.
+#' @param myDesign Vector of 1s and 2s specifying which columns in intOnly belong to the control group (1) and which belong to the test group (2).
+#' @param intOnly A data frame. Each column is a sample, each row is an analyte.
+#' @param nPerms The number of permutations to perform. At least 100 is recommended.
+#' @keywords p-values, FDP, FDR, permutation
 #' @export
 #' @examples
 #' cat_function()
 
-# This function controls FDR using the permutation method described in our manuscript. Like the BH method above, it corrects the rejection threshold rather than the p-values themselves.
-# It returns the new threshold for P-value rejection.
-# It uses the Rcpp and BH packages to leverage fast C++ code.
-permFDP.adjust.threshold = function(pVals, threshold, myDesign, intOnly, nPerms, nc, nt) {
+permFDP.adjust.threshold = function(pVals, threshold, myDesign, intOnly, nPerms) {
   pVals = pVals[order(pVals)]
   intMatrix = as.matrix(intOnly)
+  nc = length(which(myDesign == 1))
+  nt = length(which(myDesign == 2))
   return(permFDRAdjustCpp(pVals, threshold, myDesign, intMatrix, nPerms, nc, nt))
 }
