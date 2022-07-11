480e6f9c0f3602004562068520d89a9dcd3db0e5
steven-shuken <srshuken@gmail.com>
2022-07-11 16:29:44 -0400

    Ran the code in document.R. There was a problem with BH because I didn't add it to the LinkingTo: field in DESCRIPTION.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -10,6 +10,52 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
+// designTTest
+double designTTest(std::vector<double> ints, std::vector<int> design);
+RcppExport SEXP _RcppPackage_designTTest(SEXP intsSEXP, SEXP designSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< std::vector<double> >::type ints(intsSEXP);
+    Rcpp::traits::input_parameter< std::vector<int> >::type design(designSEXP);
+    rcpp_result_gen = Rcpp::wrap(designTTest(ints, design));
+    return rcpp_result_gen;
+END_RCPP
+}
+// permFDRAdjust
+double permFDRAdjust(std::vector<double> expPs, double threshold, std::vector<int> design, std::vector<std::vector<double>> intOnly, int nPerms, int nc, int nt);
+RcppExport SEXP _RcppPackage_permFDRAdjust(SEXP expPsSEXP, SEXP thresholdSEXP, SEXP designSEXP, SEXP intOnlySEXP, SEXP nPermsSEXP, SEXP ncSEXP, SEXP ntSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< std::vector<double> >::type expPs(expPsSEXP);
+    Rcpp::traits::input_parameter< double >::type threshold(thresholdSEXP);
+    Rcpp::traits::input_parameter< std::vector<int> >::type design(designSEXP);
+    Rcpp::traits::input_parameter< std::vector<std::vector<double>> >::type intOnly(intOnlySEXP);
+    Rcpp::traits::input_parameter< int >::type nPerms(nPermsSEXP);
+    Rcpp::traits::input_parameter< int >::type nc(ncSEXP);
+    Rcpp::traits::input_parameter< int >::type nt(ntSEXP);
+    rcpp_result_gen = Rcpp::wrap(permFDRAdjust(expPs, threshold, design, intOnly, nPerms, nc, nt));
+    return rcpp_result_gen;
+END_RCPP
+}
+// permFDRAdjustCpp
+double permFDRAdjustCpp(Rcpp::NumericVector expPs, double threshold, Rcpp::NumericVector design, Rcpp::NumericMatrix intMatrix, int nPerms, int nc, int nt);
+RcppExport SEXP _RcppPackage_permFDRAdjustCpp(SEXP expPsSEXP, SEXP thresholdSEXP, SEXP designSEXP, SEXP intMatrixSEXP, SEXP nPermsSEXP, SEXP ncSEXP, SEXP ntSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type expPs(expPsSEXP);
+    Rcpp::traits::input_parameter< double >::type threshold(thresholdSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type design(designSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type intMatrix(intMatrixSEXP);
+    Rcpp::traits::input_parameter< int >::type nPerms(nPermsSEXP);
+    Rcpp::traits::input_parameter< int >::type nc(ncSEXP);
+    Rcpp::traits::input_parameter< int >::type nt(ntSEXP);
+    rcpp_result_gen = Rcpp::wrap(permFDRAdjustCpp(expPs, threshold, design, intMatrix, nPerms, nc, nt));
+    return rcpp_result_gen;
+END_RCPP
+}
 // rcpp_hello_world
 List rcpp_hello_world();
 RcppExport SEXP _RcppPackage_rcpp_hello_world() {
@@ -22,6 +68,9 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_RcppPackage_designTTest", (DL_FUNC) &_RcppPackage_designTTest, 2},
+    {"_RcppPackage_permFDRAdjust", (DL_FUNC) &_RcppPackage_permFDRAdjust, 7},
+    {"_RcppPackage_permFDRAdjustCpp", (DL_FUNC) &_RcppPackage_permFDRAdjustCpp, 7},
     {"_RcppPackage_rcpp_hello_world", (DL_FUNC) &_RcppPackage_rcpp_hello_world, 0},
     {NULL, NULL, 0}
 };
