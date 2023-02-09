1da068e10bace45fbd7f040a85ab0e5756547fe3
Dirk Eddelbuettel <edd@debian.org>
2023-02-09 07:56:16 -0600

    Run compileAttributes() to update exports list

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -56,22 +56,11 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpp_hello_world
-List rcpp_hello_world();
-RcppExport SEXP _permFDP_rcpp_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpp_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_permFDP_designTTest", (DL_FUNC) &_permFDP_designTTest, 2},
     {"_permFDP_permFDRAdjust", (DL_FUNC) &_permFDP_permFDRAdjust, 7},
     {"_permFDP_permFDRAdjustCpp", (DL_FUNC) &_permFDP_permFDRAdjustCpp, 7},
-    {"_permFDP_rcpp_hello_world", (DL_FUNC) &_permFDP_rcpp_hello_world, 0},
     {NULL, NULL, 0}
 };
 
